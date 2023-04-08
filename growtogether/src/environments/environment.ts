c13020c18964f4757c8f689846de180563bb3440
joaobnd <joao.martins@ccc.ufcg.edu.br>
2023-04-08 00:08:41 -0300

    connect with firebase succesfuly

---
+++ growtogether/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBU1Y93sRzIQ4CKRz0auuqlTdg5_vA3B2g",
+    authDomain: "growtogether-31c5e.firebaseapp.com",
+    projectId: "growtogether-31c5e",
+    storageBucket: "growtogether-31c5e.appspot.com",
+    messagingSenderId: "883177798828",
+    appId: "1:883177798828:web:620b99b3b93896e3007309",
+    measurementId: "G-6CBLS0MZVX"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
