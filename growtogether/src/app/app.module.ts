7634dcd051bee31754ab5b35d3845daa59d748e6
joaobnd <joao.martins@ccc.ufcg.edu.br>
2023-04-11 02:58:02 -0300

    succesfuly install bootstrap

--- growtogether/src/app/app.module.ts
+++ growtogether/src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule} from '@angular/fire/compat'
 import { environment } from 'src/environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
