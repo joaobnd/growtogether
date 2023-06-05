5616815d8a6fb22952fd0ee51f7e97096527441b
joaobnd <joao.martins@ccc.ufcg.edu.br>
2023-06-05 00:50:27 -0300

    update environment

--- growtogether/src/environments/environment.prod.ts
+++ growtogether/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBU1Y93sRzIQ4CKRz0auuqlTdg5_vA3B2g",
+    authDomain: "growtogether-31c5e.firebaseapp.com",
+    projectId: "growtogether-31c5e",
+    storageBucket: "growtogether-31c5e.appspot.com",
+    messagingSenderId: "883177798828",
+    appId: "1:883177798828:web:620b99b3b93896e3007309",
+    measurementId: "G-6CBLS0MZVX"
+  }
 };
